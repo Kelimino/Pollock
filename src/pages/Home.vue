afdb17eae0554f1110a7c64a48648e6b78a148e1
Kellig JOURDREN - USEWEB <kellig.jourdren@MacKellig.local>
2019-08-29 10:33:45 +0200

    init

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -26,14 +26,9 @@ import { ScrollMagicPluginGsap } from 'scrollmagic-plugin-gsap'
 
 ScrollMagicPluginGsap(ScrollMagic, TimelineMax)
 
-
 export default {
 
-
   mounted () {
-
-
-
     var controller = new ScrollMagic.Controller()
 
     var tl = new TimelineMax()
