59d202bc3a22fc240b7f367ac4a0b7bb3d0fff8b
Kellig JOURDREN - USEWEB <kellig.jourdren@MacKellig.local>
2019-08-29 18:55:49 +0200

    init

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
-
-      <img src="" alt="logo" class="logo">
-      <div class="nav-button">
-
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+    <img src alt="logo" class="logo" />
+    <div class="nav-button">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
 
     <div class="menu">
       <ul>
@@ -17,56 +15,57 @@
       </ul>
     </div>
 
-    <img src="@/assets/img/back.jpg" alt="back" class="back">
-    <img src="@/assets/img/logo.png" alt="background">
+    <img src="@/assets/img/back.jpg" alt="back" class="back" />
+    <img src="@/assets/img/logo.png" alt="background" />
 
     <div class="title">
-    <h1>Hello world</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, pariatur eius exercitationem
-      iusto facere sapiente iste neque cum dolore sunt laborum ad illum nobis eum quam deserunt?
-      Veritatis, dolorum voluptates!
-    </p>
-
+      <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, pariatur eius exercitationem
+        iusto facere sapiente iste neque cum dolore sunt laborum ad illum nobis eum quam deserunt?
+        Veritatis, dolorum voluptates!
+      </p>
     </div>
   </div>
 </template>
 <script>
 import ScrollMagic from 'scrollmagic'
-import { TweenMax } from 'gsap'
+import { TimelineMax, Power1 } from 'gsap'
 import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'
 import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js'
-require()
 
 export default {
   mounted () {
     /// menu-open
+    var navBtn = document.querySelector('.nav-button')
 
-    let menuOpen = new TweenMax({paused: true})
+    let tl = new TimelineMax({ paused: true })
 
-    menuOpen.to('.menu', 1.6, {
+    tl.to('.menu', 1.6, { top: 0, ease: Power1.easeInOut })
+      .staggerFrom('.menu ul li', { y: 100, opacity: 0, ease: Power1.easeOut })
+      .reverse()
 
-      top: 0,
-      ease: Power1.easeInOut
-
+    navBtn.on('click', (e) => {
+      if (tl.isActive()) {
+        e.preventDefault()
+        e.stopPropagation()
+        return false
+      }
     })
 
-    menuOpen.staggerFrom('.menu ul li', { y: 100, opacity: 0, ease: Power1.easeOut})
-
-    menuOpen.reverse()
-
-    $(document).on('click', '.nav-button', function () {
-      menuOpen.reversed(!menuOpen.reversed())
-    })
+    // end menu
 
     let controller = new ScrollMagic.Controller()
 
     // scene1
 
-    let MainTitle = new TweenMax()
-    MainTitle
-      .staggerFrom('.title h1', 1, { opacity: 0, y: -20 })
-      .staggerFrom('.title p', 1, { opacity: 0, y: -20 }, '-=.8')
+    let MainTitle = new TimelineMax()
+    MainTitle.staggerFrom('.title h1', 1, { opacity: 0, y: -20 }).staggerFrom(
+      '.title p',
+      1,
+      { opacity: 0, y: -20 },
+      '-=.8'
+    )
 
     new ScrollMagic.Scene({
       triggerElement: '.title',
