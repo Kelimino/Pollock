6bbde53d5ebbb068452685a4bb2ceb0c8f5df2ec
Kellig JOURDREN - USEWEB <kellig.jourdren@MacKellig.local>
2019-08-29 18:20:03 +0200

    init

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
 
+      <img src="" alt="logo" class="logo">
+      <div class="nav-button">
+
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+    <div class="menu">
+      <ul>
+        <li>Work</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+
+    <img src="@/assets/img/back.jpg" alt="back" class="back">
+    <img src="@/assets/img/logo.png" alt="background">
+
     <div class="title">
-    <h1>hello world</h1>
+    <h1>Hello world</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, pariatur eius exercitationem
       iusto facere sapiente iste neque cum dolore sunt laborum ad illum nobis eum quam deserunt?
@@ -10,60 +29,51 @@
     </p>
 
     </div>
-
-    <div id="stage">
-      <div class="box box1">1</div>
-      <div class="box box2">2</div>
-      <div class="box box3">3</div>
-      <div class="box box4">4</div>
-      <div class="box box5">5</div>
-      <div class="box box6">6</div>
-    </div>
   </div>
 </template>
 <script>
 import ScrollMagic from 'scrollmagic'
-import { TimelineMax } from 'gsap'
+import { TweenMax } from 'gsap'
 import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'
 import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js'
+require()
 
 export default {
   mounted () {
+    /// menu-open
+
+    let menuOpen = new TweenMax({paused: true})
+
+    menuOpen.to('.menu', 1.6, {
+
+      top: 0,
+      ease: Power1.easeInOut
+
+    })
+
+    menuOpen.staggerFrom('.menu ul li', { y: 100, opacity: 0, ease: Power1.easeOut})
+
+    menuOpen.reverse()
+
+    $(document).on('click', '.nav-button', function () {
+      menuOpen.reversed(!menuOpen.reversed())
+    })
+
     let controller = new ScrollMagic.Controller()
 
     // scene1
 
-    let MainTitle = new TimelineMax()
+    let MainTitle = new TweenMax()
     MainTitle
       .staggerFrom('.title h1', 1, { opacity: 0, y: -20 })
-      .staggerFrom('.title p', 1, {opacity: 0,y: -20}, "+=0.5")
+      .staggerFrom('.title p', 1, { opacity: 0, y: -20 }, '-=.8')
 
     new ScrollMagic.Scene({
       triggerElement: '.title',
-      offset: 200
+
+      offset: 0
     })
       .setTween(MainTitle)
-      .addIndicators()
-      .addTo(controller)
-
-    // scene2
-    let tl = new TimelineMax()
-    tl.staggerFrom('.box', 1.25, {
-      scale: 0,
-      cycle: { y: [-50, 50] },
-      stagger: { from: 'center', amount: 0.25 }
-    })
-
-    new ScrollMagic.Scene({
-      triggerElement: '#stage',
-      triggerHook: 0,
-      offset: -100,
-      duration: 30
-    })
-
-      .setPin('#stage')
-      .setClassToggle('.box1', 'large')
-      .setTween(tl)
       .addIndicators()
       .addTo(controller)
   }
