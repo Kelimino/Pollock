b6b8525f8bf8abf6eb6179fe263db4b37a9201fb
Kellig JOURDREN - USEWEB <kellig.jourdren@MacKellig.local>
2019-09-02 12:19:35 +0200

    init

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,9 @@
   <div container-fluid>
     <div class="cursor"></div>
 
-    <div class="logo"><img src="@/assets/img/logo.png" alt=""></div>
+    <div class="logo">
+      <img src="@/assets/img/logo.png" alt />
+    </div>
     <div class="nav-button" v-on:click.stop.prevent="open = !open" v-bind:class="{open:open}">
       <span></span>
       <span></span>
@@ -21,7 +23,7 @@
       <audio id="sound" autoplay controls hidden loop preload="true">
         <source src="@/assets/sound.mp3" type="audio/mpeg" />
       </audio>
-      <button type="button" class="mute"></button>
+      <a class="mute"></a>
     </div>
 
     <img src="@/assets/img/back3.jpg" alt="back" class="back" />
@@ -90,171 +92,171 @@
   </div>
 </template>
 <script>
-import ScrollMagic from 'scrollmagic'
-import { TimelineLite, Expo, Power2 } from 'gsap/TweenMax'
-import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'
-import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js'
+import ScrollMagic from "scrollmagic";
+import { TimelineLite, Expo, Power2 } from "gsap/TweenMax";
+import "scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js";
+import "scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js";
 
 export default {
-  data () {
+  data() {
     return {
       open: false
-    }
+    };
   },
 
-  mounted () {
+  mounted() {
     // audio
-    var audioTrack = document.querySelector('#sound')
-    var audioControl = document.querySelector('#player .mute')
-    function initAudio () {
-      audioTrack.volume = 0.05
-      audioTrack.muted = false
-      audioTrack.loop = true
-      audioTrack.play()
+    var audioTrack = document.querySelector("#sound");
+    var audioControl = document.querySelector("#player .mute");
+    function initAudio() {
+      audioTrack.volume = 0.05;
+      audioTrack.muted = false;
+      audioTrack.loop = true;
+      audioTrack.play();
     }
 
-    audioControl.addEventListener('click', muteAudio)
-    function muteAudio () {
+    audioControl.addEventListener("click", muteAudio);
+    function muteAudio() {
       if (audioTrack.muted) {
-        audioTrack.muted = false
+        audioTrack.muted = false;
       } else {
-        audioTrack.muted = true
+        audioTrack.muted = true;
       }
     }
-    window.addEventListener('load', initAudio)
+    window.addEventListener("load", initAudio);
 
     // cursor
-    const cursor = document.querySelector('.cursor')
+    const cursor = document.querySelector(".cursor");
 
-    document.addEventListener('mousemove', e => {
+    document.addEventListener("mousemove", e => {
       cursor.setAttribute(
-        'style',
-        'top:' + e.pageY + 'px; left:' + e.pageX + 'px;'
-      )
-    })
+        "style",
+        "top:" + e.pageY + "px; left:" + e.pageX + "px;"
+      );
+    });
 
-    const animalHover = document.querySelector('.animal-wrap .animal')
+    const animalHover = document.querySelector(".animal-wrap .animal");
 
-    animalHover.addEventListener('mouseenter', e => {
-      console.log(cursor)
+    animalHover.addEventListener("mouseenter", e => {
+      console.log(cursor);
 
-      cursor.style.width = '100px'
-    })
+      cursor.style.width = "100px";
+    });
 
     //  menu-open
-    const navBtn = document.querySelector('.nav-button')
-    const userList = document.querySelectorAll('.menu ul li')
+    const navBtn = document.querySelector(".nav-button");
+    const userList = document.querySelectorAll(".menu ul li");
 
-    const tl = new TimelineLite({ paused: true })
+    const tl = new TimelineLite({ paused: true });
 
-    tl.to('.menu', 1.1, { height: '100%', ease: Expo.easeInOut })
+    tl.to(".menu", 1.1, { height: "100%", ease: Expo.easeInOut })
       .staggerFrom(
-        '.menu ul li',
+        ".menu ul li",
         0.5,
         { y: -100, opacity: 0, ease: Power2.easeOut },
-        '-= 1.1'
+        "-= 1.1"
       )
-      .reverse()
+      .reverse();
 
-    navBtn.addEventListener('click', e => {
+    navBtn.addEventListener("click", e => {
       if (tl.isActive()) {
-        e.preventDefault()
-        e.stopPropagation()
-        return false
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
       }
-      tl.reversed(!tl.reversed())
-    })
+      tl.reversed(!tl.reversed());
+    });
 
     for (let user of userList) {
-      user.addEventListener('click', e => {
+      user.addEventListener("click", e => {
         if (tl.isActive()) {
-          e.preventDefault()
-          e.stopPropagation()
-          return false
+          e.preventDefault();
+          e.stopPropagation();
+          return false;
         }
-        tl.reversed(!tl.reversed())
-        if (navBtn.classList.contains('open')) {
-          navBtn.classList.remove('open')
+        tl.reversed(!tl.reversed());
+        if (navBtn.classList.contains("open")) {
+          navBtn.classList.remove("open");
         } else {
-          navBtn.classList.add('open')
+          navBtn.classList.add("open");
         }
-      })
+      });
     }
 
     // end menu
 
     // scene1
 
-    const MainTitle = new TimelineLite({ onComplete: onComplete })
-    const exploreBtn = document.querySelector('.explore')
+    const MainTitle = new TimelineLite({ onComplete: onComplete });
+    const exploreBtn = document.querySelector(".explore");
 
-    MainTitle.to('.back', 12, { scale: 1.2, ease: Power2.easeOut })
+    MainTitle.to(".back", 12, { scale: 1.2, ease: Power2.easeOut })
       .fromTo(
-        '.title h1',
+        ".title h1",
         2,
         { opacity: 0, y: -20, ease: Power2.easeOut },
         { opacity: 1, y: 0 },
-        '-= 11'
+        "-= 11"
       )
       .fromTo(
-        '.title h2',
+        ".title h2",
         2,
         { opacity: 0, y: -20, ease: Power2.easeOut },
         { opacity: 1, y: 0 },
-        '-= 11'
+        "-= 11"
       )
-      .to('.title h2', 2, { opacity: 0, y: 20, ease: Power2.easeOut }, '-= 9')
-      .to('.title h1', 2, { opacity: 0, y: 20, ease: Power2.easeOut }, '-= 9')
+      .to(".title h2", 2, { opacity: 0, y: 20, ease: Power2.easeOut }, "-= 9")
+      .to(".title h1", 2, { opacity: 0, y: 20, ease: Power2.easeOut }, "-= 9")
 
       .fromTo(
-        '.title p',
+        ".title p",
         2,
         { opacity: 0, y: -20, ease: Power2.easeOut },
         { opacity: 1, y: 0 },
-        '-= 8'
+        "-= 8"
       )
-      .to('.title p', 2, { opacity: 0, y: 20, ease: Power2.easeOut }, '-= 6')
+      .to(".title p", 2, { opacity: 0, y: 20, ease: Power2.easeOut }, "-= 6")
       .fromTo(
         exploreBtn,
         2,
         { opacity: 0, y: -20, ease: Power2.easeOut },
         { opacity: 1, y: 0 },
-        '-= 5'
-      )
+        "-= 5"
+      );
 
-    function onComplete () {
-      const tl2 = new TimelineLite({ paused: true })
+    function onComplete() {
+      const tl2 = new TimelineLite({ paused: true });
       tl2
         .to(exploreBtn, 1.2, { autoAlpha: 0, y: -20, ease: Expo.easeInOut })
-        .to('.back', 2, { scale: 1.1, ease: Expo.easeInOut })
+        .to(".back", 2, { scale: 1.1, ease: Expo.easeInOut })
         .from(
-          '.animal-wrap',
+          ".animal-wrap",
           2,
           { autoAlpha: 0, y: -20, ease: Expo.easeInOut },
-          '-= 0.5'
+          "-= 0.5"
         )
         .set(exploreBtn, { x: -100, y: 100, z: -20 })
-        .reverse()
+        .reverse();
 
-      exploreBtn.addEventListener('click', e => {
+      exploreBtn.addEventListener("click", e => {
         if (tl2.isActive()) {
-          e.preventDefault()
-          e.stopPropagation()
-          return false
+          e.preventDefault();
+          e.stopPropagation();
+          return false;
         }
-        tl2.reversed(!tl2.reversed())
-      })
+        tl2.reversed(!tl2.reversed());
+      });
     }
 
-    const controller = new ScrollMagic.Controller()
+    const controller = new ScrollMagic.Controller();
     new ScrollMagic.Scene({
-      triggerElement: 'nav-button'
-    }).addTo(controller)
+      triggerElement: "nav-button"
+    }).addTo(controller);
 
-    const covertDetail = document.querySelector('.animal-detail')
-    const detailContent = document.querySelector('.animal-detail .row')
-    const animalProfile = document.querySelector('.animal-wrap .animal')
-    const timeDetail = new TimelineLite({ paused: true })
+    const covertDetail = document.querySelector(".animal-detail");
+    const detailContent = document.querySelector(".animal-detail .row");
+    const animalProfile = document.querySelector(".animal-wrap .animal");
+    const timeDetail = new TimelineLite({ paused: true });
 
     timeDetail
       .from(covertDetail, 2, { autoAlpha: 0, ease: Power2.easeOut })
@@ -262,21 +264,21 @@ export default {
         detailContent,
         2,
         { autoAlpha: 0, scale: 1.2, ease: Power2.easeOut },
-        ' -= 0.5'
-      )
+        " -= 0.5"
+      );
 
-    animalProfile.addEventListener('click', function () {
-      timeDetail.play()
-    })
+    animalProfile.addEventListener("click", function() {
+      timeDetail.play();
+    });
 
-    window.addEventListener('click', e => {
+    window.addEventListener("click", e => {
       if (timeDetail.isActive()) {
-        e.preventDefault()
-        e.stopPropagation()
-        return false
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
       }
-      timeDetail.reversed(!timeDetail.reversed())
-    })
+      timeDetail.reversed(!timeDetail.reversed());
+    });
   }
-}
+};
 </script>
