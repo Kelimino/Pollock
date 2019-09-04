b6023c3fdeedab9247d53db1c8c534036132bbe8
Kellig JOURDREN - USEWEB <kellig.jourdren@MacKellig.local>
2019-09-04 16:21:55 +0200

    init

--- src/main.js
+++ src/main.js
@@ -8,8 +8,9 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
-require('@/assets/css/main.scss')
+
 require('@/assets/css/app.scss')
+require('@/assets/css/main.scss')
 
 Vue.config.productionTip = false
 
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,17 +1,62 @@
 <template>
-
-    <div>
-
+  <div class="viewport">
+    <div id="scroll-container" class="scroll-container">
+      <div class="content">
         <h1>About Me</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, pariatur eius exercitationem
-            iusto facere sapiente iste neque cum dolore sunt laborum ad illum nobis eum quam deserunt?
-            Veritatis, dolorum voluptates!</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, pariatur eius exercitationem
+          iusto facere sapiente iste neque cum dolore sunt laborum ad illum nobis eum quam deserunt?
+          Veritatis, dolorum voluptates!
+        </p>
+
+        <div class="img-container">
+          <img src="@/assets/img/pol1.jpg" alt />
+        </div>
+        <div class="img-container">
+          <img src="@/assets/img/pol2.jpg" alt />
+        </div>
+        <div class="img-container">
+          <img src="@/assets/img/pol3.jpg" alt />
+        </div>
+        <div class="img-container">
+          <img src="@/assets/img/pol4.jpg" alt />
+        </div>
+        <!--  REPEAT  -->
+
+       <div class="img-container">
+          <img src="@/assets/img/pol1.jpg" alt />
+        </div>
+        <div class="img-container">
+          <img src="@/assets/img/pol2.jpg" alt />
+        </div>
+        <div class="img-container">
+          <img src="@/assets/img/pol3.jpg" alt />
+        </div>
+        <div class="img-container">
+          <img src="@/assets/img/pol4.jpg" alt />
+        </div>
+        <!--  REPEAT  -->
+
+<div class="img-container">
+          <img src="@/assets/img/pol1.jpg" alt />
+        </div>
+        <div class="img-container">
+          <img src="@/assets/img/pol2.jpg" alt />
+        </div>
+        <div class="img-container">
+          <img src="@/assets/img/pol3.jpg" alt />
+        </div>
+        <div class="img-container">
+          <img src="@/assets/img/pol4.jpg" alt />
+        </div>
+        <!--  REPEAT  -->
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'About'
-
-}
+  name: "About"
+};
 </script>
